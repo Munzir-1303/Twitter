705147a14b7af5921ec66b5f2ce1389f5fe95529
Munzir-1303 <usanpubgbot@gmail.com>
2022-12-07 13:49:58 +0300

    Twitter

---
+++ js/main.js
@@ -0,0 +1,121 @@
+let likeclick = document.querySelector('.hero__center--chat2img2link2222');
+let likewhite = document.querySelector('.hero__center--chat2img2222');
+let likered = document.querySelector('.hero__center--chat2img3');
+let likeresult = document.querySelector('.hero__center--chat2img2spanlike');
+let clicked = false;
+
+likeclick.addEventListener('click', () => {
+    if(!clicked){
+        clicked = true;
+        likewhite.style.display = 'none';
+        likered.style.display = 'block';
+        likeresult.textContent++;
+    }else{
+        clicked = false;
+        likewhite.style.display = 'block';
+        likered.style.display = 'none';
+        likeresult.textContent--;
+    }
+});
+
+let likeclick3 = document.querySelector('.hero__center--chat2img2link22223');
+let likewhite3 = document.querySelector('.hero__center--chat2img22223');
+let likered3 = document.querySelector('.hero__center--chat2img33');
+let likeresult3 = document.querySelector('.hero__center--chat2img2spanlike3');
+let clicked3 = false;
+
+likeclick.addEventListener('click', () => {
+    if(!clicked3){
+        clicked3 = true;
+        likewhite3.style.display = 'none';
+        likered3.style.display = 'block';
+        likeresult3.textContent++;
+    }else{
+        clicked3 = false;
+        likewhite3.style.display = 'block';
+        likered3.style.display = 'none';
+        likeresult3.textContent--;
+    }
+});
+
+let likeclick4 = document.querySelector('.hero__center--chat2img2link22224');
+let likewhite4 = document.querySelector('.hero__center--chat2img22224');
+let likered4 = document.querySelector('.hero__center--chat2img34');
+let likeresult4 = document.querySelector('.hero__center--chat2img2spanlike4');
+let clicked4 = false;
+
+likeclick.addEventListener('click', () => {
+    if(!clicked4){
+        clicked4 = true;
+        likewhite4.style.display = 'none';
+        likered4.style.display = 'block';
+        likeresult4.textContent++;
+    }else{
+        clicked4 = false;
+        likewhite4.style.display = 'block';
+        likered4.style.display = 'none';
+        likeresult4.textContent--;
+    }
+});
+
+
+let repaetclick = document.querySelector('.hero__center--chat2img2linkrepaet');
+let repaetwhite = document.querySelector('.hero__center--chat2img2repaet');
+let repaetred = document.querySelector('.hero__center--chat2img2repaet2');
+let result = document.querySelector('.hero__center--chat2img2spanrepaet');
+let clickedrepaet = false;
+
+repaetclick.addEventListener('click', () => {
+    if(!clickedrepaet){
+        clickedrepaet = true;
+        repaetwhite.style.display = 'none';
+        repaetred.style.display = 'block';
+        result.textContent++;
+    }else{
+        clickedrepaet = false;
+        repaetwhite.style.display = 'block';
+        repaetred.style.display = 'none';
+        result.textContent--;
+    }
+});
+
+
+let repaetclick3 = document.querySelector('.hero__center--chat2img2linkrepaet3333');
+let repaetwhite3 = document.querySelector('.hero__center--chat2img2repaet333');
+let repaetred3 = document.querySelector('.hero__center--chat2img2repaet33');
+let result3 = document.querySelector('.hero__center--chat2img2spanrepaet3');
+let clickedrepaet3 = false;
+
+repaetclick.addEventListener('click', () => {
+    if(!clickedrepaet3){
+        clickedrepaet3 = true;
+        repaetwhite3.style.display = 'none';
+        repaetred3.style.display = 'block';
+        result3.textContent++;
+    }else{
+        clickedrepaet3 = false;
+        repaetwhite3.style.display = 'block';
+        repaetred3.style.display = 'none';
+        result3.textContent--;
+    }
+});
+
+let repaetclick5 = document.querySelector('.hero__center--chat2img2linkrepaet3335');
+let repaetwhite5 = document.querySelector('.hero__center--chat2img2repaet335');
+let repaetred5 = document.querySelector('.hero__center--chat2img2repaet35');
+let result5 = document.querySelector('.hero__center--chat2img2spanrepaet5');
+let clickedrepaet5 = false;
+
+repaetclick.addEventListener('click', () => {
+    if(!clickedrepaet5){
+        clickedrepaet5 = true;
+        repaetwhite5.style.display = 'none';
+        repaetred5.style.display = 'block';
+        result5.textContent++;
+    }else{
+        clickedrepaet5 = false;
+        repaetwhite5.style.display = 'block';
+        repaetred5.style.display = 'none';
+        result5.textContent--;
+    }
+});
